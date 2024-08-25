734a203e1036315fc63e35bac8c5dd7e90122b79
jhums04 <jbonganciso04@gmail.com>
2024-08-26 04:21:10 +0800

    implemented loader functionality

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -16,24 +16,83 @@ const Registration: React.FC<RegistrationProps> = ({
   const [email, setEmailAddress] = useState("");
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState("USER");
+  const [redirect, setRedirect] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   if (isLoggedIn) {
     return <Navigate to="/home" />;
   }
 
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    const formDiv = document.getElementById(
+      "registration__form__container__id"
+    );
+    formDiv?.setAttribute("style", "display: none");
+    try {
+      const response = await axios.post("http://localhost:8080/register", {
+        firstName,
+        lastName,
+        email,
+        username,
+        password,
+        role,
+      });
+
+      if (response.data != null) {
+        setFirstName("");
+        setLastName("");
+        setEmailAddress("");
+        setUserName("");
+        setPassword("");
+        setRole("USER");
+      }
+      const loadingScreenHeader = document.getElementById(
+        "loading__screen__h2"
+      );
+
+      if (loadingScreenHeader != null) {
+        console.log("loadingScreenHeader ", loadingScreenHeader);
+        loadingScreenHeader.textContent =
+          "Please activate your account via email. Redirecting to homepage...";
+      }
+
+      setTimeout(() => {
+        setLoading(false);
+        setRedirect(true);
+      }, 10000);
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
-    <div className="registration__main__container">
-      <div className="registration__form__container">
+    <div
+      className="registration__main__container"
+      id="registration__main__container__id"
+    >
+      {loading && (
+        <div className="loading__screen">
+          <h2 id="loading__screen__h2">Submitting Registration</h2>
+          <div className="custom-loader"></div>
+        </div>
+      )}
+      <div
+        className="registration__form__container"
+        id="registration__form__container__id"
+      >
         <h1>SIGNUP</h1>
-        <form>
+        <form method="post" onSubmit={handleSubmit}>
           <div className="registration_main__input__container">
             <div className="registration__input__container">
               <div className="fullname__container">
                 <div>
                   <input
                     type="text"
-                    name="username"
-                    id="username"
+                    name="firstName"
+                    id="firstName"
                     className="input__fields input__fields__fullname"
                     placeholder="First Name"
                     value={firstName}
@@ -100,7 +159,11 @@ const Registration: React.FC<RegistrationProps> = ({
 
               <div className="input__container__registration">
                 <div>
-                  <select name="role">
+                  <select
+                    name="role"
+                    value={role}
+                    onChange={(event) => setRole(event.target.value)}
+                  >
                     <option value="USER">User</option>
                     <option value="ADMIN">Admin</option>
                   </select>
@@ -109,12 +172,15 @@ const Registration: React.FC<RegistrationProps> = ({
               </div>
 
               <div className="input__container__registration">
-                <button type="submit">SUBMIT</button>
+                <button type="submit" onSubmit={handleSubmit}>
+                  SUBMIT
+                </button>
               </div>
             </div>
           </div>
         </form>
       </div>
+      {redirect && <Navigate to="/login" />}
     </div>
   );
 };
